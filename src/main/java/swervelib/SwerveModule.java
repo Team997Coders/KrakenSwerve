cfe1d72d24d39cf4953b68ad2e8ef12c17c6830f
Strimbu <strimbub@oregonstate.edu>
2025-11-22 19:57:59 -0800

    TalonFX completly now, in SwerveModule

--- src/main/java/swervelib/SwerveModule.java
+++ src/main/java/swervelib/SwerveModule.java
@@ -1,137 +1,161 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// the WPILib BSD license file in the root directsory of this project.
 
 package swervelib;
-
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.DriveConstants.SwervePID;
+
+// copy pasted 
+
+import com.ctre.phoenix6.hardware.TalonFX; // For the Talon FX device class
+import com.ctre.phoenix6.controls.*;      // For control requests (DutyCycleOut, PositionVoltage, etc.)
+import com.ctre.phoenix6.configs.*;        // For configuration objects (TalonFXConfiguration, etc.)
+import com.ctre.phoenix6.signals.*;        // For signals and enums (InvertedValue, NeutralMode, etc.)
+import com.ctre.phoenix6.StatusCode;       // For checking API status
+import com.ctre.phoenix6.CANBus;         // For general CAN bus status
+import com.ctre.phoenix6.mechanisms.swerve.*; // For swerve drive specific classes
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 /** Add your docs here. */
 public class SwerveModule {
-  private SparkMax angleMotor;
-  private SparkMax speedMotor;
-  private RelativeEncoder speedEncoder;
-  private PIDController pidController;
-  private SparkAbsoluteEncoder encoder;
-  private double maxVelocity;
-  private double maxVoltage;
-
-  public SwerveModule(int angleMotorId, int speedMotorId, boolean driveMotorReversed, boolean angleMotorReversed,
-      boolean angleEncoderReversed, double angleEncoderConversionFactor, double angleEncoderOffset,
-      double maxVelocity, double maxVoltage) {
-    this.angleMotor = new SparkMax(angleMotorId, MotorType.kBrushless);
-    this.speedMotor = new SparkMax(speedMotorId, MotorType.kBrushless);
-
-    //this.angleMotor.restoreFactoryDefaults();
-    //this.speedMotor.restoreFactoryDefaults();
-
-    this.pidController = new PIDController(SwervePID.p, SwervePID.i, SwervePID.d);
-    this.encoder = this.angleMotor.getAbsoluteEncoder();
-    this.maxVelocity = maxVelocity;
-    this.maxVoltage = maxVoltage;
-
-    this.pidController.enableContinuousInput(-180, 180);
-
-    double driveReduction = 1.0 / 6.75;
-    double WHEEL_DIAMETER = 0.1016;
-    double rotationsToDistance = driveReduction * WHEEL_DIAMETER * Math.PI;
-
-    SparkBaseConfig angleMotorConfig = new SparkMaxConfig();
-        angleMotorConfig
-          .inverted(angleMotorReversed)
-          .idleMode(IdleMode.kBrake);
-        angleMotorConfig.absoluteEncoder
-          .positionConversionFactor(1)
-          .velocityConversionFactor(1)
-          .inverted(angleEncoderReversed);
-
-    SparkBaseConfig speedMotorConfig = new SparkMaxConfig();
-        speedMotorConfig
-          .inverted(driveMotorReversed)
-          .idleMode(IdleMode.kBrake);
-        speedMotorConfig.encoder
-          .positionConversionFactor(rotationsToDistance)
-          .velocityConversionFactor(rotationsToDistance/60);
-
-    this.speedEncoder = this.speedMotor.getEncoder();
+    private static final double gearRatio = 150.0; // check this number
+    private TalonFX angleMotor;
+    private TalonFX speedMotor;
+    private PIDController pidController;
+    private double maxVelocity;
+    private double maxVoltage;
+  
+    private double WHEEL_DIAMETER = 8;
+    private double wheelCirc = Math.PI * WHEEL_DIAMETER;
+    private double driveReduction = 1.0 / 6.75;
     
-    //angleMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
-    //speedMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+  
+    public SwerveModule(int angleMotorId, int speedMotorId, boolean driveMotorReversed, boolean angleMotorReversed,
+        boolean angleEncoderReversed, double angleEncoderConversionFactor, double angleEncoderOffset,
+        double maxVelocity, double maxVoltage) {
+      this.angleMotor = new TalonFX(angleMotorId);
+      this.speedMotor = new TalonFX(speedMotorId);
+  
+      //this.angleMotor.restoreFactoryDefaults();
+      //this.speedMotor.restoreFactoryDefaults();
+  
+      this.pidController = new PIDController(SwervePID.p, SwervePID.i, SwervePID.d);
+     
+      this.maxVelocity = maxVelocity;
+      this.maxVoltage = maxVoltage;
+      
+  
+      this.pidController.enableContinuousInput(-180, 180);
+  
+      double driveReduction = 1.0 / 6.75;
+      double WHEEL_DIAMETER = 0.1016;
+    
+  
+      TalonFXConfiguration angleConfig = new TalonFXConfiguration();
+      TalonFXConfiguration driveConfig = new TalonFXConfiguration();
+      
+      // angle
+      angleConfig.MotorOutput.Inverted =
+          angleMotorReversed ? InvertedValue.Clockwise_Positive
+                             : InvertedValue.CounterClockwise_Positive;
+      
+      // drive
+      driveConfig.MotorOutput.Inverted =
+          driveMotorReversed ? InvertedValue.Clockwise_Positive
+                             : InvertedValue.CounterClockwise_Positive;
+      
+      angleMotor.getConfigurator().apply(angleConfig);
+      speedMotor.getConfigurator().apply(driveConfig);
+      //angleMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+      //speedMotor.setSmartCurrentLimit(DriveConstants.currentLimit);
+  
+      //this.encoder.setZeroOffset(angleEncoderOffset);
+  
 
-    //this.encoder.setZeroOffset(angleEncoderOffset);
+  
+    }
+  
+    
+  
+    public SwerveModule(SwerveModuleConfig config, double maxVelocity, double maxVoltage) {
+      this(config.angleMotorId,
+          config.driveMotorId,
+          config.driveMotorReversed,
+          config.angleMotorReversed,
+          config.angleEncoderReversed,
+          config.angleEncoderConversionFactor,
+          config.angleEncoderOffset,
+          maxVelocity,
+          maxVoltage);
+    }
+  
+    /**
+     * drive:
+     * 
+     * @param speedMetersPerSecond
+     * @param angle
+     *                             Basic drive code. Lots of debug information sent
+     *                             to the dashboard so that
+     *                             we can watch what is happening. Remember that
+     *                             everything should be CCW positive.
+     */
+    private void drive(double speedMetersPerSecond, double angle) {
+      double drive_voltage = (speedMetersPerSecond / maxVelocity) * maxVoltage;
+      double angle_voltage = pidController.calculate(this.getEncoder(), angle);
+  
+      speedMotor.setVoltage(drive_voltage);
+      angleMotor.setVoltage(angle_voltage);
+    }
+  
+    /**
+     * drive:
+     * 
+     * @param state of the module (velocity and angle)
+     */
+    public void drive(SwerveModuleState state) {
+      state.optimize(getRotation());
+  
+      // a little wierd logic. Call the other 'drive' code above to actually move the
+      // module.
+      this.drive(state.speedMetersPerSecond, state.angle.getDegrees());
+    }
+  
+    /**
+     * getEncoder:
+     * 
+     * 
+     *       
+     */
+  
+    public double getEncoder() {
+    return angleMotor.getPosition().getValueAsDouble() * (360.0 / gearRatio);
   }
 
-  public SwerveModule(SwerveModuleConfig config, double maxVelocity, double maxVoltage) {
-    this(config.angleMotorId,
-        config.driveMotorId,
-        config.driveMotorReversed,
-        config.angleMotorReversed,
-        config.angleEncoderReversed,
-        config.angleEncoderConversionFactor,
-        config.angleEncoderOffset,
-        maxVelocity,
-        maxVoltage);
-  }
 
-  /**
-   * drive:
-   * 
-   * @param speedMetersPerSecond
-   * @param angle
-   *                             Basic drive code. Lots of debug information sent
-   *                             to the dashboard so that
-   *                             we can watch what is happening. Remember that
-   *                             everything should be CCW positive.
-   */
-  private void drive(double speedMetersPerSecond, double angle) {
-    double drive_voltage = (speedMetersPerSecond / maxVelocity) * maxVoltage;
-    double angle_voltage = pidController.calculate(this.getEncoder(), angle);
-
-    speedMotor.setVoltage(drive_voltage);
-    angleMotor.setVoltage(angle_voltage);
-  }
-
-  /**
-   * drive:
-   * 
-   * @param state of the module (velocity and angle)
-   */
-  public void drive(SwerveModuleState state) {
-    state.optimize(getRotation());
-
-    // a little wierd logic. Call the other 'drive' code above to actually move the
-    // module.
-    this.drive(state.speedMetersPerSecond, state.angle.getDegrees());
-  }
-
-  /**
-   * getEncoder:
-   * 
-   * @return Return the module angle in degrees 0-360. CCW positive.
-   *         Straight Forward should be 0 (with the addjustment of module offset)
-   */
-  public double getEncoder() {
-    return encoder.getPosition() * 360.0;
-  }
 
   /*
    * Return the applied voltage on the drive motor (0-12V)
    */
   public double getDriveOutput() {
-    return speedMotor.getAppliedOutput();
+    double dutyCycle = speedMotor.getDutyCycle().getValueAsDouble();
+    double busVoltage = speedMotor.getSupplyVoltage().getValueAsDouble();
+
+    
+    return dutyCycle * busVoltage; 
   }
 
   /*
@@ -154,13 +178,21 @@ public class SwerveModule {
    * return the valid location in meters.
    */
   public SwerveModulePosition getPosition() {
-    return new SwerveModulePosition(speedEncoder.getPosition(), getRotation());
+    double driveRotations = speedMotor.getPosition().getValueAsDouble();
+    double driveMeters = driveRotations * wheelCirc * driveReduction;
+
+    return new SwerveModulePosition(driveMeters, getRotation());
+
+
   }
 
   /*
    * Another view of the module state, showing velocity instead of position
    */
   public SwerveModuleState getState() {
-    return new SwerveModuleState(speedEncoder.getVelocity(), getRotation());
+  double driveRPS = speedMotor.getVelocity().getValueAsDouble();
+  double driveMPS = driveRPS * wheelCirc * driveReduction;
+
+  return new SwerveModuleState(driveMPS, getRotation());
   }
 }
